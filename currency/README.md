3ebeb8528e3e21185ebe4a5646f6e312464baa3e
capucineROHART <56581907+capucineROHART@users.noreply.github.com>
2020-03-03 16:54:23 +0100

    Readme & comments done

--- currency/README.md
+++ currency/README.md
@@ -1,1 +1,15 @@
-# Currency 
+# Currency
+
+##INSTALLATION
+
+#Need to install npm in the file where the library is saved
+	--> cd path/to/file
+	--> npm install
+
+
+#Run cli.js file in order to use the library. This file runs directly index.js & index.test.js
+
+
+##LIBRARY'S USAGE
+
+#Converts US Dollars into Bitcoins
--- currency/index.js
+++ currency/index.js
@@ -1,28 +1,29 @@
-const axios = require('axios');
+const axios = require('axios'); //calls for existing libraries
 const money = require('money');
 
-const RATES_URL = 'https://api.exchangeratesapi.io/latest';
+const RATES_URL = 'https://api.exchangeratesapi.io/latest'; //stores urls where the conversion rates are saved
 const BLOCKCHAIN_URL = 'https://blockchain.info/ticker';
 const CURRENCY_BITCOIN = 'BTC';
 
 const isAnyBTC = (from, to) => [from, to].includes(CURRENCY_BITCOIN);
 
-module.exports = async opts => {
+module.exports = async opts => { //function called in cli.js
   const {amount = 1, from = 'USD', to = CURRENCY_BITCOIN} = opts;
   const promises = [];
   let base = from;
 
   const anyBTC = isAnyBTC(from, to);
 
-  if (anyBTC) {
-    base = from === CURRENCY_BITCOIN ? to : from;
-    promises.push(axios(BLOCKCHAIN_URL));
+  if (anyBTC) { //if we need to convert bitcoins in one way or another
+    base = from === CURRENCY_BITCOIN ? to : from; //if from is bitcoin, then to is USD ; else from is bitcoin
+    //bitcoin is mandatory one way or another
+    promises.push(axios(BLOCKCHAIN_URL)); //stores the conversion rates found into an array
   }
 
-  promises.unshift(axios(`${RATES_URL}?base=${base}`));
+  promises.unshift(axios(`${RATES_URL}?base=${base}`)); //checks the rate on the website for the currency chosen 
 
   try {
-    const responses = await Promise.all(promises);
+    const responses = await Promise.all(promises); //waits for responses from the website
     const [rates] = responses;
 
     money.base = rates.data.base;
@@ -43,7 +44,7 @@ module.exports = async opts => {
       });
     }
 
-    if (anyBTC) {
+    if (anyBTC) { //if both currency are defined, assign values to variables
       Object.assign(conversionOpts, {
         'from': to,
         'to': from
@@ -51,7 +52,7 @@ module.exports = async opts => {
     }
 
     return money.convert(amount, conversionOpts);
-  } catch (error) {
+  } catch (error) { //if error occured
     throw new Error (
       '💵 Please specify a valid `from` and/or `to` currency value!'
     );
